967c474e03d76bb92798363dadee0b288e0f90d2
Bruno Klein <bruno.klein@digitaldesk.com.br>
2017-12-04 15:24:10 -0200

    footer e navbar

--- app/components/app.js
+++ app/components/app.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Footer from './footer';
+import Navbar from './Navbar';
 
 class App extends React.Component {
   render() {
     return (
       <div>
+        <Navbar history={this.props.history} />
         {this.props.children}
+        <Footer />
       </div>
     );
   }
--- server.js
+++ server.js
@@ -10,6 +10,9 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var Character = require('./models/character');
+var config = require('./config');
 
 var app = express();
 
@@ -35,6 +38,29 @@ app.use(function(req, res) {
   });
 });
 
-app.listen(app.get('port'), function() {
+mongoose.connect(config.database);
+mongoose.connection.on('error', function() {
+  console.info('Error: Could not connect to MongoDB. Did you forget to run `mongod`?');
+});
+
+/**
+ * Socket.io stuff.
+ */
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var onlineUsers = 0;
+
+io.sockets.on('connection', function(socket) {
+  onlineUsers++;
+
+  io.sockets.emit('onlineUsers', { onlineUsers: onlineUsers });
+
+  socket.on('disconnect', function() {
+    onlineUsers--;
+    io.sockets.emit('onlineUsers', { onlineUsers: onlineUsers });
+  });
+});
+
+server.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
 });
